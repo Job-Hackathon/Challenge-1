0c71cea2dbe81151823086effc5fc9fbafdb1fb0
oglis22 <119761510+oglis22@users.noreply.github.com>
2024-07-14 12:10:48 +0200

    logik of close airports

--- Backend/router.js
+++ Backend/router.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { close_airports, close_airports } = require('./apifetcher');
 
-router.get('/', (req, res) => {
+router.post('/api/v1/close-airports', (req, res) => {
+
+    const { originairportcode, maxradius, startdate } = req.body;
+    const ca = close_airports()
 
 });
 
