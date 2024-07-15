0afdf84c0dd97f08b63fd2460376db955473e651
Geoffrey Karnbach <geoffrey.karnbach@gmail.com>
2024-07-15 15:23:28 +0300

    Finished weather display for a given target location

--- Frontend/src/app/components/shared/weather-modal/weather-modal.component.ts
+++ Frontend/src/app/components/shared/weather-modal/weather-modal.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { WeatherModalDataDto } from '../../../dtos';
+import { WeatherForecastDto, WeatherModalDataDto } from '../../../dtos';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-weather-modal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './weather-modal.component.html',
   styleUrl: './weather-modal.component.scss'
 })
@@ -27,5 +28,35 @@ export class WeatherModalComponent implements OnInit{
     forecast: []
   }
 
+  roundToNearestInteger(value: number): number {
+    return Math.round(value);
+  }
+
+  formatDate(date: string): string {
+    const dateObj = new Date(date);
+
+    const day = dateObj.toLocaleDateString('en-US', { weekday: 'long' });
+    const month = dateObj.toLocaleDateString('en-US', { month: 'long' });
+    const dayOfMonth = dateObj.getDate();
+    const year = dateObj.getFullYear();
+
+    return `${day}, ${month} ${dayOfMonth}, ${year}`;
+  }
+
+  getWeatherIconFromData(data: WeatherForecastDto): string {
+    const { sunshineDuration, rainSum, showersSum, snowfallSum, uvIndexMax } = data;
+
+    if (snowfallSum > 0) {
+      return "bi-snow";
+    } else if (rainSum > 0 || showersSum > 0) {
+      return "bi-cloud-rain";
+    } else if (sunshineDuration > 0.75 * data.daylightDuration && uvIndexMax > 5) {
+      return "bi-sun";
+    } else if (sunshineDuration < 0.25 * data.daylightDuration) {
+      return "bi-cloud";
+    } else {
+      return "bi-cloud-sun";
+    }
+  }
 
 }
