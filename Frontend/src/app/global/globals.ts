fbd59c5b4c3621e6b3343b04733d2a9b639701ce
Geoffrey Karnbach <geoffrey.karnbach@gmail.com>
2024-07-15 16:06:25 +0300

    Changed API route for deployment

--- Frontend/src/app/global/globals.ts
+++ Frontend/src/app/global/globals.ts
@@ -7,15 +7,12 @@ export class Globals {
   readonly backendUri: string = this.findBackendUrl();
 
   private findBackendUrl(): string {
-    return 'http://localhost:4000/api/v1';
-    /**
+
     if (window.location.port === '4200') {
       // local `ng serve`, backend at localhost:8080
       return 'http://localhost:4000/api/v1';
     } else {
-      // assume deployed somewhere and backend is available at same host/port as frontend
-      return window.location.protocol + '//' + window.location.host + window.location.pathname + 'api/v1';
+      return 'http://45.93.250.193:4000/api/v1';
     }
-      */
   }
 }
