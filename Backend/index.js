25610e719fc8d301e2cdd3feb4c5e6c0ebe2c959
Geoffrey Karnbach <geoffrey.karnbach@gmail.com>
2024-07-15 16:10:34 +0300

    Fixed origin port

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 
 express()
   .use(cors({
-    origin: 'http://45.93.250.193',
+    origin: 'http://45.93.250.193:4000',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }))
   .use(bodyParser.json())
